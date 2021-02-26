efbe6e1ab9f657b581c2f32e061e7121854a93c7
Sangwoo96 <nsw5296@naver.com>
2021-02-26 18:11:05 +0900

    [Add] 빈 스코프 작업 중

---
+++ 스프링 핵심 원리/core/src/main/java/dev/highright96/core/web/LogDemoService.java
@@ -0,0 +1,19 @@
+package dev.highright96.core.web;
+
+import dev.highright96.core.common.MyLogger;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LogDemoService {
+
+    private final MyLogger myLogger;
+    //private final ObjectProvider<MyLogger> myLoggerProvider;
+
+    public void logic(String id) {
+        //MyLogger myLogger = myLoggerProvider.getObject();
+        myLogger.log("service id = " + id);
+    }
+}
